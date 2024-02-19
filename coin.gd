dede06ac491772cc492187efe0311167a655174a
Noah <nordqvist.gaming@gmail.com>
2024-02-19 18:20:56 +0100

    randomize coin shine animation

--- coin.gd
+++ coin.gd
@@ -3,6 +3,11 @@ extends Area2D
 var screen_size: Vector2 = Vector2.ZERO
 
 
+func _ready() -> void:
+    $Timer.timeout.connect(_on_timer_timeout)
+    $Timer.start(randf_range(0.5, 1.5))
+
+
 func pickup() -> void:
     $CollisionShape2D.set_deferred("disabled", true)
     var tween = create_tween().set_parallel().set_trans(Tween.TRANS_QUAD)
@@ -10,4 +15,9 @@ func pickup() -> void:
     tween.tween_property(self, "modulate:a", 0.0, 0.2)
     await tween.finished
     queue_free()
+
+
+func _on_timer_timeout() -> void:
+    $AnimatedSprite2D.frame = 0
+    $AnimatedSprite2D.play()
 
